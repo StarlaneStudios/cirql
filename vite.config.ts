5dd7537e0117fcd1c92a8777c5d0c04bb348101a
Julian Mills <julian.exodius@gmail.com>
2023-01-12 17:58:52 +0100

    release: version 0.1.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(e => ({
+	publicDir: e.command == 'serve' ? 'public' : false,
 	build: {
 		lib: {
 			entry: './lib/index.ts',
@@ -17,4 +18,4 @@ export default defineConfig({
 			},
 		}
 	}
-})
+}))
