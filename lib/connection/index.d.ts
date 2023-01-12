8537692b6aabfe49fef1f4cbabeaaa8cc9880944
Julian Mills <julian.exodius@gmail.com>
2023-01-12 01:47:05 +0100

    fix: remove incorrect declaration files

--- lib/connection/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { SurrealOptions, SurrealHandle } from './types';
-/**
- * Create a new Surreal connection
- *
- * @param options The connection options
- * @returns A handle to the connection
- */
-export declare function openConnection(options: SurrealOptions): SurrealHandle;
